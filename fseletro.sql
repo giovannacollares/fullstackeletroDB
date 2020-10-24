45b798bf0ca09ca07f2a0181ead77924a3fe3d5e
Giovanna Collares <giovannacollares@gmail.com>
2020-10-24 15:11:05 -0300

    commit fseletrodb

---
+++ fseletro.sql
@@ -0,0 +1,70 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Linux (x86_64)
+--
+-- Host: localhost    Database: fseletro
+-- ------------------------------------------------------
+-- Server version	8.0.21-0ubuntu0.20.04.4
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `pedidos`
+--
+
+DROP TABLE IF EXISTS `pedidos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pedidos` (
+  `idpedido` int NOT NULL AUTO_INCREMENT,
+  `nomecompleto` varchar(45) NOT NULL,
+  `endereco` varchar(50) NOT NULL,
+  `complemento` varchar(50) DEFAULT NULL,
+  `telefone` bigint DEFAULT NULL,
+  `produto` int DEFAULT NULL,
+  `descricaopedido` varchar(150) NOT NULL,
+  `quantidade` int NOT NULL,
+  `valorunitario` decimal(8,2) NOT NULL,
+  `valortotal` decimal(8,2) NOT NULL,
+  PRIMARY KEY (`idpedido`),
+  KEY `produto` (`produto`),
+  CONSTRAINT `pedidos_ibfk_1` FOREIGN KEY (`produto`) REFERENCES `produtos` (`idproduto`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `produtos`
+--
+
+DROP TABLE IF EXISTS `produtos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `produtos` (
+  `idproduto` int NOT NULL AUTO_INCREMENT,
+  `categoria` varchar(45) NOT NULL,
+  `descricao` varchar(150) NOT NULL,
+  `preco` decimal(8,2) DEFAULT NULL,
+  `precofinal` decimal(8,2) DEFAULT NULL,
+  `imagens` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`idproduto`),
+  UNIQUE KEY `imagens` (`imagens`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-10-24 15:02:08
